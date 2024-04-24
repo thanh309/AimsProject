70ace4dd40364ebad5921f852147dc160a2915cb
user <vuthnh.td@gmail.com>
2024-04-24 11:15:29 +0700

    temp

--- src/TestPassingParameter.java
+++ src/TestPassingParameter.java
@@ -5,20 +5,19 @@ public class TestPassingParameter {
         DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
         DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
 
-        DigitalVideoDisc[] discs = {jungleDVD, cinderellaDVD};
-        swap(discs);
+        swap(jungleDVD, cinderellaDVD);
 
-        System.out.println("jungle dvd title: " + discs[0].getTitle());
-        System.out.println("cinderella dvd title: " + discs[1].getTitle());
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+        System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
 
         changeTitle(jungleDVD, cinderellaDVD.getTitle());
         System.out.println("jungle dvd title: " + jungleDVD.getTitle());
     }
 
-    public static void swap(Object[] objects) {
-        Object temp = objects[0];
-        objects[0] = objects[1];
-        objects[1] = temp;
+    public static void swap(Object o1, Object o2) {
+        Object tmp = o1;
+        o1 = o2;
+        o2 = tmp;
     }
 
     public static void changeTitle(DigitalVideoDisc dvd, String title) {
