8c9f892b1ad6dfa2d7f93b14249799c661b581e6
thanh309 <vuthnh.td@gmail.com>
2024-05-15 11:31:35 +0700

    5.3 completed

--- src/hust/soict/dsai/aims/media/CompactDisc.java
+++ src/hust/soict/dsai/aims/media/CompactDisc.java
@@ -1,4 +1,43 @@
 package hust.soict.dsai.aims.media;
 
+import java.util.List;
+
 public class CompactDisc extends Disc {
+    private String artist;
+    private List<Track> tracks;
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public CompactDisc(String artist) {
+        this.artist = artist;
+    }
+
+    public void addTrack(Track track) {
+        if (tracks.contains(track)) {
+            System.out.println("Track is already in list");
+            return;
+        }
+        tracks.add(track);
+        System.out.println("Track added");
+    }
+
+    public void removeTrack(Track track) {
+        if (tracks.remove(track)) {
+            System.out.println("Track removed");
+            return;
+        }
+        System.out.println("Track is not in list");
+    }
+
+    @Override
+    public int getLength() {
+        int result = 0;
+        for (Track track : tracks) {
+            result += track.getLength();
+        }
+        return result;
+    }
 }
+
