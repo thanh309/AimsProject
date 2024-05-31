b596fc65cf1cc2759c735c24e269e4965df0cfae
thanh309 <149485828+thanh309@users.noreply.github.com>
2024-06-01 06:54:37 +0700

    9. finished

--- src/hust/soict/dsai/aims/screen/controller/CartScreenController.java
+++ src/hust/soict/dsai/aims/screen/controller/CartScreenController.java
@@ -3,8 +3,7 @@ package hust.soict.dsai.aims.screen.controller;
 import hust.soict.dsai.aims.cart.Cart;
 import hust.soict.dsai.aims.media.Media;
 import hust.soict.dsai.aims.media.Playable;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
@@ -56,6 +55,12 @@ public class CartScreenController {
                         updateButtonBar(newValue);
                     }
                 });
+    }
+
+    @FXML
+    void btnRemovePressed(ActionEvent event) {
+        Media media = tblMedia.getSelectionModel().getSelectedItem();
+        cart.removeMedia(media);
     }
 
     void updateButtonBar(Media media) {
